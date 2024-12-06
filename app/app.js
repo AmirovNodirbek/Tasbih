566a506ae980ccefef3fcff4d74acbd7f601a681
AmirovNodirbek <nodiramerov5@gmail.com>
2024-12-06 15:44:58 +0300

    Nodirbek

---
+++ app/app.js
@@ -0,0 +1,6 @@
+const yurak = document.querySelector(".head")
+const red = document.querySelector(".red")
+function addHeart(){
+    yurak.classList.toggle("active_head")
+    red.classList.toggle("active_red")
+}
